84ed1d54546f834437f477c93b458e092140804d
ana-kennedy <167390708+ana-kennedy@users.noreply.github.com>
2024-04-19 17:03:56 -0500

    Add files via upload

---
+++ rosterscript.js
@@ -0,0 +1,57 @@
+document.getElementById('button1').addEventListener('click' , function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+
+document.querySelector('.close').addEventListener('click' , function(){
+document.querySelector('.bg-modal').style.display = 'none' ;
+});
+
+
+document.getElementById('button2').addEventListener('click' , function() {
+    document.querySelector('.bg-modal2').style.display = 'flex';
+});
+
+document.querySelector('.close2').addEventListener('click' , function(){
+document.querySelector('.bg-modal2').style.display = 'none' ;
+});
+
+
+document.getElementById('button3').addEventListener('click' , function() {
+    document.querySelector('.bg-modal3').style.display = 'flex';
+});
+
+document.querySelector('.close3').addEventListener('click' , function(){
+document.querySelector('.bg-modal3').style.display = 'none' ;
+});
+
+document.getElementById('button4').addEventListener('click' , function() {
+    document.querySelector('.bg-modal4').style.display = 'flex';
+});
+
+
+document.querySelector('.close4').addEventListener('click' , function(){
+document.querySelector('.bg-modal4').style.display = 'none' ;
+});
+
+
+document.getElementById('button5').addEventListener('click' , function() {
+    document.querySelector('.bg-modal5').style.display = 'flex';
+});
+
+
+document.querySelector('.close5').addEventListener('click' , function(){
+document.querySelector('.bg-modal5').style.display = 'none' ;
+});
+
+
+
+document.getElementById('button6').addEventListener('click' , function() {
+    document.querySelector('.bg-modal6').style.display = 'flex';
+});
+
+
+document.querySelector('.close6').addEventListener('click' , function(){
+document.querySelector('.bg-modal6').style.display = 'none' ;
+});
+
