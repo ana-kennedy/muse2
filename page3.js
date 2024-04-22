97708408f9929aafa0512d511ad495e0bd2a8220
Thomas Domville <thomasdomville@Anas-Macbook.local>
2024-04-22 17:21:40 -0500

    ‘update01’

---
+++ page3.js
@@ -0,0 +1,17 @@
+document.getElementById('button1').addEventListener('click' , function() {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+
+
+document.querySelector('.close').addEventListener('click' , function(){
+document.querySelector('.bg-modal').style.display = 'none' ;
+});
+
+document.getElementById('button2').addEventListener('click' , function() {
+    document.querySelector('.bg-modal2').style.display = 'flex';
+});
+
+
+document.querySelector('.close2').addEventListener('click' , function(){
+document.querySelector('.bg-modal2').style.display = 'none' ;
+});
